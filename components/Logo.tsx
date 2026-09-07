36054bb8cee15d01c0cfda60681e2ec7ed311209
turek4578-alt <turek4578@gmail.com>
2026-09-07 15:50:08 +0200

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -10,7 +10,7 @@
 export function LogoMark({ size = 26 }: { size?: number }) {
   return (
     <span
-      className="OK"
+      className="ok"
       style={{
         width: size,
         height: size,
